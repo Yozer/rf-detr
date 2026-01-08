e24ef1753ba291707233d876aacbeb29ce4a164a
Dominik Baran <dominik.baran7@gmail.com>
2026-01-08 19:14:33 +0100

    wandb

--- rfdetr/config.py
+++ rfdetr/config.py
@@ -274,6 +274,7 @@ class TrainConfig(BaseModel):
     early_stopping_use_ema: bool = False
     tensorboard: bool = True
     wandb: bool = False
+    wandb_train_log_freq: int = 0
     project: Optional[str] = None
     run: Optional[str] = None
     class_names: List[str] = None
